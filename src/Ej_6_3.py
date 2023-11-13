3b52d7eb3967b5b3b680bc0be2df7317fddfcf1d
Ejercicios_DAW_Python <aarrieta@iesrafaelalberti.es>
2023-11-13 08:35:15 +0100

    Comentarios y variables iniciales de los ejercicios

--- src/Ej_6_3.py
+++ src/Ej_6_3.py
@@ -1,6 +1,18 @@
+"""
+Ejercicio 3.3.6¶
+
+Dado el conjunto de letras:
+vocales = {'a', 'e', 'i', 'o', 'u'}
+
+ - Crea un conjunto consonantes que contenga las letras del alfabeto que no son vocales.
+ - Crea un conjunto letras_comunes que contenga las letras que están tanto en el conjunto vocales como en el conjunto
+ consonantes.
+"""
+
+
 def function(example: str) -> str:
     return None
 
 
 if __name__ == "__main__":
-    ""
+    vocales = {'a', 'e', 'i', 'o', 'u'}
